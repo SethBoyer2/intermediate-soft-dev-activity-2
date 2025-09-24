73d2e0108ae85a03c1017916b8066a6da391478b
Seth <sboyer2@academic.rrc.ca>
2025-09-24 16:27:48 -0500

    Third commit: Completed triangle.py unit tests

--- shape/triangle.py
+++ shape/triangle.py
@@ -34,58 +34,47 @@ class Triangle(Shape):
         else:
             raise ValueError("Side 3 must be numeric.")
 
-        if side_2 + side_3 > side_1:
-            self.__side_1 = side_1
-        else:
-            raise ValueError("Invalid length")
+        if not (side_2 + side_3 > side_1 and
+                side_1 + side_3 > side_2 and
+                side_1 + side_2 > side_3):
+            raise ValueError(
+                "Please ensure all sides adhere to the triangle inequality theorem.")
 
-        if side_1 + side_3 > side_2:
-            self.__side_2 = side_2
-        else:
-            raise ValueError("Invalid length")
+    def __str__(self) -> str:
+        return (
+            super().__str__() + f"\n This triangle has three sides" +
+            f" with lengths of {self.__side_1}, {self.__side_2} and {self.__side_3}.")
 
-        if side_1 + side_2 > side_3:
-            self.__side_3 = side_3
-        else:
-            raise ValueError("Invalid length")
+    def calculate_area(self) -> float:
+        """
+        This method takes three validated arguments, and uses
+        them to calculate the area of the shape
 
-        def __str__(self) -> str:
-            return (
-                super().__str__() + f"\n This triangle has three sides" +
-                f" with lengths of {self.side_1}, {self.side_2} and {self.side_3}.")
+        Args:
+        side_1(int) : side 1 of the triangle
+        side_2(int) : side 2 of the triangle
+        side_3(int) : side 3 of the triangle
 
-        @abstractmethod
-        def calculate_area(self, side_1: int, side_2: int, side_3: int) -> float:
-            """
-            This method takes three validated arguments, and uses
-            them to calculate the area of the shape
+        Returns:
+        Area(float) : The area of the shape
+        """
+        sp = (self.__side_1 + self.__side_2 + self.__side_3)/2
+        self.area = math.sqrt(sp * (sp - self.__side_1) *
+                              (sp - self.__side_2) * (sp - self.__side_3))
+        self.area = round(self.area, 2)
 
-            Args:
-            side_1(int) : side 1 of the triangle
-            side_2(int) : side 2 of the triangle
-            side_3(int) : side 3 of the triangle
+    def calculate_perimeter(self) -> float:
+        """
+        This method takes three validated arguments and returns
+        the sum as the perimeter of the shape
 
-            Returns:
-            Area(float) : The area of the shape
-            """
-            sp = (self.__side_1 + self.__side_2 + self.__side_3)/2
-            area = math.sqrt(sp * (sp - self.__side_1) *
-                             (sp - self.__side_2) * (sp - self.__side_3))
-            return area
+        Args:
+        side_1(int) : side 1 of the triangle
+        side_2(int) : side 2 of the triangle
+        side_3(int) : side 3 of the triangle
 
-        @abstractmethod
-        def calculate_perimeter(self, side_1: int, side_2: int, side_3: int) -> float:
-            """
-            This method takes three validated arguments and returns
-            the sum as the perimeter of the shape
-
-            Args:
-            side_1(int) : side 1 of the triangle
-            side_2(int) : side 2 of the triangle
-            side_3(int) : side 3 of the triangle
-
-            Returns:
-            Perimeter(int): the sum of all three sides.
-            """
-            perimeter = (side_1 + side_2 + side_3)
-            return perimeter
+        Returns:
+        Perimeter(int): the sum of all three sides.
+        """
+        self.perimeter = (self.__side_1 + self.__side_2 + self.__side_3)
+        self.perimeter = round(self.perimeter)
--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,12 +1,58 @@
 """This module defines the TestTriangle class.
 
-Usage: 
+Usage:
     To execute all tests in the terminal execute the following command:
 
     $ python -m unittest tests/test_triangle.py
 """
 
-__author__ = ""
-__version__ = ""
+__author__ = "Seth Boyer"
+__version__ = "1.0.0"
 
 import unittest
+from shape.shape import Shape
+from shape.triangle import Triangle
+
+
+class TestClient(unittest.TestCase):
+    def test_triangle_created_with_input_values(self):
+        triangle = Triangle("red", 12, 12, 12)
+
+        self.assertEqual("red", triangle._color)
+
+    def test_exception_raised_when_color_blank(self):
+        with self.assertRaises(ValueError):
+            triangle = Triangle("", 12, 12, 12)
+
+    def test_exception_raised_when_side_1_not_integer(self):
+        with self.assertRaises(ValueError):
+            triangle = Triangle("red", "hi", 12, 12)
+
+    def test_exception_raised_when_side_2_not_integer(self):
+        with self.assertRaises(ValueError):
+            triangle = Triangle("red", 12, "hi", 12)
+
+    def test_exception_raised_when_side_3_not_integer(self):
+        with self.assertRaises(ValueError):
+            triangle = Triangle("red", 12, 12, "hi")
+
+    def setUp(self):
+        self.triangle = Triangle("Red", 12, 12, 12)
+
+    def test_str_returns_expected_output(self):
+        expected = (
+            Shape.__str__(self.triangle) + f"\n This triangle has three sides" +
+            f" with lengths of {self.triangle._Triangle__side_1}, {self.triangle._Triangle__side_2}"
+            and f"{self.triangle._Triangle__side_3}."
+        )
+        self.assertEqual(expected, str(self.triangle))
+
+    def test_calculate_area_returns_correct_value(self):
+        expected = 62.35
+        self.triangle.calculate_area()
+        self.assertEqual(self.triangle.area, expected)
+
+    def test_calculate_perimeter_returns_correct_value(self):
+        expected = 36
+        self.triangle.calculate_perimeter()
+        self.assertEqual(self.triangle.perimeter, expected)
